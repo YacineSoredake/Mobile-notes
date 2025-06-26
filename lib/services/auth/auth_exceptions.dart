5bb33d31fa6fa8f108248f416af50c5059024d55
unknown <blahayacine9@gmail.com>
2025-06-26 14:09:35 +0100

    features

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,7 @@
+class UserNotFoundAuthException implements Exception {}
+class WrongPasswordAuthException implements Exception {}
+
+// register exepcetions
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUseAuthException implements Exception {} 
+class InvalidEmailAuthException implements Exception {}
