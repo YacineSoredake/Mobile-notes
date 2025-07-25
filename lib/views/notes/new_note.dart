d4e56ca11ff489d549a59377b48f8aedda3024c8
unknown <blahayacine9@gmail.com>
2025-07-26 00:37:45 +0100

    add note view

--- lib/views/notes/new_note.dart
+++ lib/views/notes/new_note.dart
@@ -14,6 +14,27 @@ class _NewNoteViewState extends State<NewNoteView> {
   late final NoteService _noteService;
   late final TextEditingController _textController;
 
+  void initState() {
+    super.initState();
+    _noteService = NoteService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  void _textControllerListener() async {
+    final note = _note;
+    if (note == null) {
+      return;
+    }
+    final text = _textController.text;
+    await _noteService.updateNote(note: note, text: text);
+  }
+
+  void _setupTextController() {
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+  }
+
   Future<DatabaseNote> createNewNote() async {
     final existingNote = _note;
     if (existingNote != null) {
@@ -26,7 +47,7 @@ class _NewNoteViewState extends State<NewNoteView> {
     return await _noteService.createNote(owner: owner, text: text);
   }
 
-  void DeleteNoteIfEmpty() {
+  void _DeleteNoteIfEmpty() {
     final note = _note;
     final text = _textController.text;
     if (text.isEmpty && note != null) {
@@ -34,9 +55,59 @@ class _NewNoteViewState extends State<NewNoteView> {
     }
   }
 
+  void _SaveNoteIfNotEmpty() async {
+    final note = _note;
+    final text = _textController.text;
+    if (note != null && text.isNotEmpty) {
+      await _noteService.updateNote(note: note, text: text);
+    }
+  }
+
+  @override
+  void dispose() {
+    _DeleteNoteIfEmpty();
+    _SaveNoteIfNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final note = await createNewNote();
+              _note = note;
+              _setupTextController();
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.save),
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: createNewNote(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              _note = snapshot.data as DatabaseNote;
+              _setupTextController();
+              return TextField(
+                controller: _textController,
+                keyboardType: TextInputType.multiline,
+                decoration: const InputDecoration(
+                  hintText: 'Type your note here...',
+                ),
+                maxLines: null,
+              );
+            default:
+              return const Center(child: Text('Unexpected state'));
+          }
+        },
+      ) ,
     );
   }
 }
