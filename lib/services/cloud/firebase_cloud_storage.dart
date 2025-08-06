b5687ab5326082c707eb34aa7fe0dbfd9f96cfd8
unknown <blahayacine9@gmail.com>
2025-08-06 03:29:39 +0100

    cloud create

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,10 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_app/services/cloud/cloud_exception.dart';
+import 'package:flutter_app/services/cloud/cloud_storage_const.dart';
+import 'package:flutter_app/services/cloud/cloud_note.dart';
 
 class FirebaseCloudStorage {
   final notes = FirebaseFirestore.instance.collection('notes');
 
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
+      notes.snapshots().map(
+        (event) => event.docs
+            .map((doc) => CloudNote.fromSnapshot(doc))
+            .where((note) => note.ownerUserId == ownerUserId),
+      );
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+          .get()
+          .then(
+            (value) => value.docs.map((doc) {
+              return CloudNote(
+                documentId: doc.id,
+                ownerUserId: doc.data()[ownerUserIdFieldName],
+                text: doc.data()[textFieldName],
+              );
+            }),
+          );
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
   void createNewNote({required String ownerUserId, required String text}) {
-    notes.add({'user_id': ownerUserId, 'text': text});
+    notes.add({ownerUserIdFieldName: ownerUserId, textFieldName: 'text'});
   }
 
   static final FirebaseCloudStorage _shared =
